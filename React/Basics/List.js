193b68387e0107c6e435e118d06b4d4811a0aae5
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-05-19 20:05:50 +0530

    Update List.js

--- React/Basics/List.js
+++ React/Basics/List.js
@@ -1,1 +1,13 @@
-gfhgjkl;
+import React from "react"
+import ReactDom from "react-dom"
+function Unnati()
+{
+  return (
+    <ul>
+    <li>Paris</li>
+    <li>Italy</li>
+    <li>Germany</li>
+    </ul>
+    )
+}
+ReactDom.render(<Unnati/>,document.getElementById('root'))
